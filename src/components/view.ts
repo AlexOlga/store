097e7d60a7eb2a07c504c64908ddae3b723a65ec
AlexOlga <bulbenko@yandex.ru>
2022-07-13 22:15:51 +0300

    fix: change layout product item

--- src/components/view.ts
+++ src/components/view.ts
@@ -8,10 +8,10 @@ export class Products {
             (newsClone.querySelector('.product__img') as HTMLImageElement).src = item.urlToImage;
             (newsClone.querySelector('.product__name') as HTMLElement).textContent = item.name;
             (newsClone.querySelector('.product__description') as HTMLElement).textContent = item.description;
-            (newsClone.querySelector('.product__range') as HTMLElement).textContent = `${item.range}`;
+            (newsClone.querySelector('.product__range') as HTMLElement).textContent = `${item.range} а.е.`;
             (newsClone.querySelector('.product__time') as HTMLElement).textContent = `${item.time}`;
             (newsClone.querySelector('.product__placeVizit') as HTMLElement).textContent = item.placeVizit.join(' ');
-            (newsClone.querySelector('.product__price') as HTMLElement).textContent = '${item.price}';
+            (newsClone.querySelector('.product__price') as HTMLElement).textContent = `${item.price} у.е.`;
             fragment.append(newsClone);
         });
 
